4c51a581b473a53d8ac279228ff75edc0b2cb41d
Anthony E <e4nthony@gmail.com>
2022-11-19 23:41:56 +0200

    tests finished

---
+++ tests/post.test.js
@@ -0,0 +1,98 @@
+const request = require('supertest')
+const app = require('../server')
+const mongoose = require('mongoose')
+const Post = require('../models/post_model')
+
+const newPostMessage = 'This is the new test post message'
+const newPostSender = '999000'
+
+
+beforeAll(async ()=>{
+    await Post.remove()
+})
+
+afterAll(async ()=>{
+    await Post.remove()
+    mongoose.connection.close()
+})
+
+
+describe("Posts Tests", ()=>{
+
+    test("add new post",async ()=>{
+        const response = await request(app).post('/post').send({
+            "message": newPostMessage,
+            "sender": newPostSender
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(newPostMessage)
+        expect(response.body.sender).toEqual(newPostSender)
+
+        Post1_ID = response.body._id
+    })
+
+    test("get all posts",async ()=>{
+        const response = await request(app).get('/post')
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body[0].message).toEqual(newPostMessage)
+        expect(response.body[0].sender).toEqual(newPostSender)
+    })
+
+    test("get post by id",async ()=>{
+        const response = await request(app).get('/post/' + Post1_ID)
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(newPostMessage)
+        expect(response.body.sender).toEqual(newPostSender)
+    })
+
+    test("get post by wrong id fails",async ()=>{
+        const response = await request(app).get('/post/12345')
+
+        expect(response.statusCode).toEqual(400)
+    })
+
+    test("get post by sender",async ()=>{
+        const response = await request(app).get('/post?sender=' + newPostSender)
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body[0].message).toEqual(newPostMessage)
+        expect(response.body[0].sender).toEqual(newPostSender)
+    })
+
+    test("update post by ID",async ()=>{
+        let response = await request(app).put('/post/' + Post1_ID).send({
+            "message": Post1_MessageUpdated,
+            "sender": newPostSender
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(Post1_MessageUpdated)
+        expect(response.body.sender).toEqual(newPostSender)
+
+        //--- 
+        response = await request(app).get('/post/' + Post1_ID)
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(Post1_MessageUpdated)
+        expect(response.body.sender).toEqual(newPostSender)
+
+        //--- 
+        response = await request(app).put('/post/new1').send({
+            "message": Post1_MessageUpdated,
+            "sender": newPostSender
+        })
+        expect(response.statusCode).toEqual(400)
+
+        //--- 
+        response = await request(app).put('/post/' + Post1_ID).send({
+            "message": Post1_MessageUpdated,
+        })
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(Post1_MessageUpdated)
+        expect(response.body.sender).toEqual(newPostSender)
+    })
+
+})
