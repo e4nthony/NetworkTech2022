33c4c92a68ee054ec3f56f749f177c4b4e6a8724
Anthony E <e4nthony@gmail.com>
2023-01-09 01:48:24 +0200

    minor changes

--- routes/post_route.js
+++ routes/post_route.js
@@ -5,6 +5,7 @@ const post = require('../controllers/post.js')
 
 router.get('/',post.getAllPosts)
 
+// get post by id example: http://localhost:3000/post/61f9b38acb3146f533b3af6b
 router.get('/:id',post.getPostById)
 
 router.post('/',post.addNewPost)
