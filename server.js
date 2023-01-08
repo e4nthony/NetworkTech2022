ff109933cfbb7db4255ac16c3beee0babea79b36
Anthony E <e4nthony@gmail.com>
2023-01-08 23:39:55 +0200

    update error message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db = mongoose.connection
 db.on('error',error=>{console.error('Failed to connect to mongo DB: '+error)})
-db.once('open',()=>{console.log('connected to mongo DB')})
+db.once('open',()=>{console.log('Connected to mongo DB')})
 
 const postRouter = require('./routes/post_route.js')
 app.use('/post',postRouter)
