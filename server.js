8cb87c1f51fb0dc7b92d9b1ee64e1026050a2bba
Anthony E <e4nthony@gmail.com>
2023-01-08 23:37:47 +0200

    error print upgrade

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json())
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
 const db = mongoose.connection
-db.on('error',error=>{console.error(error)})
+db.on('error',error=>{console.error('Failed to connect to mongo DB: '+error)})
 db.once('open',()=>{console.log('connected to mongo DB')})
 
 const postRouter = require('./routes/post_route.js')
