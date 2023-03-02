401eefb0c6f29e9522fd21466e4eb579445a4b1d
Anthony E <e4nthony@gmail.com>
2023-03-02 10:51:36 +0200

    bug fixes

--- tests/post.test.js
+++ tests/post.test.js
@@ -4,15 +4,25 @@ const mongoose = require('mongoose')
 const Post = require('../models/post_model')
 
 
-const newPostMessage = 'post message 1'
-const newPostSender = '364188'
+const Post1_Message = 'post 1 message 1 - first'
+const Post1_Sender  = '000111'
+var   Post1_ID      = null
+const Post1_UpdatedMessage = 'post 1 message 2 - second'
 
 
+const Post2_Message = 'post 2 message 1 - first'
+const Post2_Sender  = '000222'
+var   Post2_ID      = null
+
+
+/**
+ * clear the DB 
+ */
 beforeAll(async ()=>{
     await Post.remove()
 })
 
-
+// clear the DB
 afterAll(async ()=>{
     await Post.remove()
     mongoose.connection.close()
@@ -22,40 +32,57 @@ afterAll(async ()=>{
 describe("Test of post actions", ()=>{
 
 
-    test("add new post",async ()=>{
+    test("add new post 1",async ()=>{
         const response = await request(app).post('/post').send({
             "message": Post1_Message,
             "sender": Post1_Sender
         })
 
         expect(response.statusCode).toEqual(200)
-        expect(response.body.message).toEqual(Post1_MessageUpdated)
+        expect(response.body.message).toEqual(Post1_Message)
         expect(response.body.sender).toEqual(Post1_Sender)
 
         Post1_ID = response.body._id
     })
 
+    test("add new post 2",async ()=>{
+        const response = await request(app).post('/post').send({
+            "message": Post2_Message,
+            "sender": Post2_Sender
+        })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.message).toEqual(Post2_Message)
+        expect(response.body.sender).toEqual(Post2_Sender)
+
+        Post2_ID = response.body._id
+    })
 
     test("get all posts",async ()=>{
         const response = await request(app).get('/post')
 
         expect(response.statusCode).toEqual(200)
-        expect(response.body[0].message).toEqual(Post1_MessageUpdated)
+
+        expect(response.body[0].message).toEqual(Post1_Message)
         expect(response.body[0].sender).toEqual(Post1_Sender)
+
+        expect(response.body[1].message).toEqual(Post2_Message)
+        expect(response.body[1].sender).toEqual(Post2_Sender)
     })
 
 
-    test("get post by id",async ()=>{
+    //get post 1 by it's id
+    test("get post by id",async ()=>{ 
         const response = await request(app).get('/post/' + Post1_ID)
 
         expect(response.statusCode).toEqual(200)
-        expect(response.body.message).toEqual(Post1_MessageUpdated)
+        expect(response.body.message).toEqual(Post1_Message)
         expect(response.body.sender).toEqual(Post1_Sender)
     })
 
 
     test("get post by wrong id fails",async ()=>{
-        const response = await request(app).get('/post/12345')
+        const response = await request(app).get('/post/123456789')
 
         expect(response.statusCode).toEqual(400)
     })
@@ -65,19 +92,19 @@ describe("Test of post actions", ()=>{
         const response = await request(app).get('/post?sender=' + Post1_Sender)
 
         expect(response.statusCode).toEqual(200)
-        expect(response.body[0].message).toEqual(Post1_MessageUpdated)
+        expect(response.body[0].message).toEqual(Post1_Message)
         expect(response.body[0].sender).toEqual(Post1_Sender)
     })
 
 
     test("update post by ID",async ()=>{
         let response = await request(app).put('/post/' + Post1_ID).send({
-            "message": Post1_MessageUpdated,
+            "message": Post1_UpdatedMessage,
             "sender": Post1_Sender
         })
 
         expect(response.statusCode).toEqual(200)
-        expect(response.body.message).toEqual(Post1_MessageUpdated)
+        expect(response.body.message).toEqual(Post1_UpdatedMessage)
         expect(response.body.sender).toEqual(Post1_Sender)
 
         //--- 
