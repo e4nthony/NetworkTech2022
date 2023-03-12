ded04197c7ebe8a126eb0afc28980045dcbcb141
Anthony E <e4nthony@gmail.com>
2023-03-12 18:41:12 +0200

    import authRouter

--- src/server.ts
+++ src/server.ts
@@ -15,6 +15,9 @@ db.once('open',()=>{console.log('Connected to MongoDB.')})
 //Make files in folder "public" accessible via url  //todo 
 app.use('/public', express.static('public'))
 
+import authRouter from './routes/auth_route.js'
+app.use('/auth',authRouter)
+
 import postRouter from './routes/post_route.js'
 app.use('/post',postRouter)
 
