6944f0e72004f42d38d704cbc37a32f71d67f874
Anthony E <e4nthony@gmail.com>
2023-03-26 02:45:23 +0300

    auth tests fix & modify

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -4,8 +4,8 @@ import mongoose from 'mongoose'
 // import auth from '../models/auth_model' //todo
 // import Post from '../models/post_model' //todo check if needed
 
-const User1_Mail = 'example@gmail.com'
-const User1_Password  = '1234'
+const User1_Mail = 'example@gmail.com';
+const User1_Password  = '1221ABcd!';
 
 // beforeAll(async ()=>{    //todo modify
 //     // await Post.remove()
@@ -17,30 +17,38 @@ const User1_Password  = '1234'
 // })
 
 
-describe("Authentication Test", ()=>{
+describe("Authentication Test", () => {
 
 
-    test("Register - add new user (user1)",async ()=>{
+    test("Register - add new user (user1)",async () => {
         const response = await request(app).post('/auth/register').send({
             "email": User1_Mail,
             "password": User1_Password
         })
-        expect(response.statusCode).toEqual(200)
+        expect(response.statusCode).toEqual(200); //no errors
     })
 
-    test("Login - login (user1)",async ()=>{
+    test("Login - (user1) Valid password",async () => {
         const response = await request(app).post('/auth/login').send({
             "email": User1_Mail,
             "password": User1_Password
         })
-        expect(response.statusCode).toEqual(200)
+        expect(response.statusCode).toEqual(200); //no errors
     })
 
-    test("Logout - logout (user1)",async ()=>{
+    test("Login - (user1) Invalid password",async () => {
+        const response = await request(app).post('/auth/login').send({
+            "email": User1_Mail,
+            "password": 'igfsmepjyfty'
+        })
+        expect(response.statusCode).toEqual(400); //wrong password
+    })
+
+    test("Logout - logout (user1)",async () => {
         const response = await request(app).post('/auth/logout').send({
             "email": User1_Mail
         })
-        expect(response.statusCode).toEqual(200)
+        expect(response.statusCode).toEqual(200);
     })
 
 
