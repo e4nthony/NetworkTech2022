956aef68a857d403ca67745adfb01aa90fec5817
Anthony E <e4nthony@gmail.com>
2023-03-26 02:30:09 +0300

    login implementation added (works, need upgrade)

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -15,36 +15,30 @@ function sendError(res: Response, error_msg: string){
 
 // requests list :
 
-const register = async (req: any, res: any) => {
-    const email = req.body.email
-    const password = req.body.password
+const register = async (req: Request, res: Response) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
     if (email == null){
-        return sendError(res, 'Epmty email')
+        return sendError(res, 'Epmty email');
     }
     else if (password == null){
-        return sendError(res, 'Epmty password')
+        return sendError(res, 'Epmty password');
     }
     //else if (password cahracters count > 16){ //todo
-    //    return sendError(res, 'Invalid password: max length = 16')
+    //    return sendError(res, 'Invalid password: max length = 16');
     //}
     // else if (email dosent have @){ //todo
-    //     return sendError(res, 'wrong email format')
+    //     return sendError(res, 'wrong email format');
     // }
 
-    // check if email in use
-    let user;
+    //  check if email in use:
     try{
-        user = await User.findOne({'email': email});    //findOne default func.
+        const user = await User.findOne({'email': email});    //findOne default func.
         if (user != null){
-            return sendError(res, 'Email already in use')
+            return sendError(res, 'Email already in use'); //already registered
         }
-    }catch(error){  // expected: server lost connection with db
-        console.log("error: " + error)  
-        return sendError(res, 'Unexpected error')
-    }
-
-    try{
+    
         //  generate hash of password:
         const salt = await bcrypt.genSalt(10);
         const encryptedPassword = await bcrypt.hash(password, salt);
@@ -55,30 +49,69 @@ const register = async (req: any, res: any) => {
             'password': encryptedPassword
         });
         await newUser.save();
-        // return res.status(statusOK).send({
-        //     'email' : email
-        //     // '_id' : newUser._id  // id of Object in DB
-        // })
-        return res.status(statusOK).send({ // Debug purposes only
-            newUser
+
+        return res.status(statusOK).send({
+            'email' : email
+            // '_id' : newUser._id  // id of Object in DB
         })
-    }catch(error){  // expected: server lost connection with db
+        // return res.status(statusOK).send({ // Debug purposes only
+        //     newUser
+        // })
+    }catch(err){  // expected: server lost connection with db
         //console.log(req.body)
-        console.log("error: " + error)  
-        return sendError(res, 'Unexpected error')
+        console.log("error: " + err);
+        return sendError(res, 'Unexpected error');
     }
 }
 
-const login = async (req: any, res: any) => {
-    res.status(statusERROR).send({'error':"Not implemented"})
+const login = async (req: Request, res: Response) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (email == null){ //todo delete double check 
+        return sendError(res, 'Epmty email');
+    }
+    else if (password == null){
+        return sendError(res, 'Epmty password');
+    }
+    //else if (password cahracters count > 16){ //todo 
+    //    return sendError(res, 'Invalid password: max length = 16');
+    //}
+    // else if (email dosent have @){ //todo
+    //     return sendError(res, 'wrong email format');
+    // }
+
+    //  check if email in use:
+    try{
+        const user = await User.findOne({'email': email});    //findOne default func.
+        if (user == null){
+            return sendError(res, 'Invalid email or password') //   Email not registered/found
+        }
+
+        //  password check:
+        const matchResult: Boolean = await bcrypt.compare(password, user.password)
+        if (matchResult == false){
+            return sendError(res, 'Invalid email or password') //   Wrong password
+        }
+
+        return res.status(statusOK).send({
+            //'email': email
+            // '_id' : newUser._id  // id of Object in DB
+            'message': 'login successful'
+        })
+    }catch(err){  // expected: server lost connection with db
+        //console.log(req.body)
+        console.log("error: " + err);
+        return sendError(res, 'Unexpected error');
+    }
 }
 
-const logout = async (req: any, res: any) => {
-    res.status(statusERROR).send({'error':"Not implemented"})
+const logout = async (req: Request, res: Response) => {
+    res.status(statusERROR).send({'error':"Not implemented"});
 }
 
 
 
-export = {register, login, logout}
+export = {register, login, logout};
 
 
