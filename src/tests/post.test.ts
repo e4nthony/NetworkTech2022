074a05a8a6a31362d05585b4265e9a0393d28862
Anthony E <e4nthony@gmail.com>
2023-03-27 01:15:40 +0300

    post tests updated - tokens

--- src/tests/post.test.ts
+++ src/tests/post.test.ts
@@ -9,12 +9,12 @@ const User1_Mail = 'example@gmail.com';
 const User1_Password = '1221ABcd!';
 let User1_id = ''  //User1_id in DB
 let User1_accessToken = '';
-// let User1_refreshToken = ''; // todo?: implement refreshed access Token check
+// let User1_refreshToken = ''; //  refreshed access Token check? -implemented in auth tests.
 
 const Post1_Message = 'post 1 message 1 - first';
 const Post1_Sender = '000111';
 let Post1_ID = null;     // post id in DB
-const Post1_UpdatedMessage = 'post 1 message 2 - second';
+const Post1_UpdatedMessage = 'post 1 message 2 (Updated) - second';
 
 
 const Post2_Message = 'post 2 message 1 - first';
@@ -24,7 +24,7 @@ let Post2_ID = null;
 
 /**
  * clear the DB
- * and create test user
+ * and register test user1
  */
 beforeAll(async () => {
     await Post.remove();
@@ -53,7 +53,8 @@ beforeEach(async () => {
  * clear the DB
  */
 afterAll(async () => {
-    await Post.remove();
+    // await Post.remove()
+    // await User.remove()
     mongoose.connection.close();
 });
 
@@ -61,7 +62,7 @@ afterAll(async () => {
 describe("Test of post actions", () => {
 
     test("add new post 1", async () => {
-        const response = await request(app).post('/post').send({
+        const response = await request(app).post('/post').set('Authorization', 'JWT' + ' ' + User1_accessToken).send({
             "message": Post1_Message,
             "sender": Post1_Sender
         });
@@ -74,7 +75,7 @@ describe("Test of post actions", () => {
     });
 
     test("add new post 2", async () => {
-        const response = await request(app).post('/post').send({
+        const response = await request(app).post('/post').set('Authorization', 'JWT' + ' ' + User1_accessToken).send({
             "message": Post2_Message,
             "sender": Post2_Sender
         });
@@ -92,9 +93,10 @@ describe("Test of post actions", () => {
 
         expect(response.statusCode).toEqual(200);
 
+        //message1
         expect(response.body[0].message).toEqual(Post1_Message);
         expect(response.body[0].sender).toEqual(Post1_Sender);
-
+        //message2
         expect(response.body[1].message).toEqual(Post2_Message);
         expect(response.body[1].sender).toEqual(Post2_Sender);
     });
@@ -103,8 +105,7 @@ describe("Test of post actions", () => {
      * gets post 1 by it's id in DB
      */
     test("get post by id", async () => {
-        // const response = await request(app).get('/post/' + Post1_ID)
-        const response = await request(app).get('/post' + Post1_ID).set('Authorization', 'JWT' + ' ' + User1_accessToken);
+        const response = await request(app).get('/post/' + Post1_ID).set('Authorization', 'JWT' + ' ' + User1_accessToken);
 
         expect(response.statusCode).toEqual(200);
         expect(response.body.message).toEqual(Post1_Message);
@@ -118,45 +119,44 @@ describe("Test of post actions", () => {
     });
 
     test("get post by sender", async () => {
-        const response = await request(app).get('/post?sender=' + Post1_Sender);
+        const response = await request(app).get('/post?sender=' + Post1_Sender).set('Authorization', 'JWT' + ' ' + User1_accessToken)
 
         expect(response.statusCode).toEqual(200);
         expect(response.body[0].message).toEqual(Post1_Message);
         expect(response.body[0].sender).toEqual(Post1_Sender);
     });
 
-    // //TODO make it work
-    // test("update post by ID",async ()=>{
-    //     let response = await request(app).put('/post/' + Post1_ID).send({
-    //         "message": Post1_UpdatedMessage,
-    //         "sender": Post1_Sender
+    //TODO make it work, wont work
+    // test("update post by ID", async () => {
+    //     let response = await request(app).put('/post/' + Post1_ID).set('Authorization', 'JWT' + ' ' + User1_accessToken).send({
+    //         "message": Post1_UpdatedMessage
     //     })
 
     //     expect(response.statusCode).toEqual(200)
     //     expect(response.body.message).toEqual(Post1_UpdatedMessage)
     //     expect(response.body.sender).toEqual(Post1_Sender)
 
-    //     //--- 
-    //     response = await request(app).get('/post/' + Post1_ID)
+    //     // //--- 
+    //     // response = await request(app).get('/post/' + Post1_ID)
 
-    //     expect(response.statusCode).toEqual(200)
-    //     expect(response.body.message).toEqual(Post1_UpdatedMessage)
-    //     expect(response.body.sender).toEqual(Post1_Sender)
+    //     // expect(response.statusCode).toEqual(200)
+    //     // expect(response.body.message).toEqual(Post1_UpdatedMessage)
+    //     // expect(response.body.sender).toEqual(Post1_Sender)
 
-    //     //--- 
-    //     response = await request(app).put('/post/new1').send({
-    //         "message": Post1_UpdatedMessage,
-    //         "sender": Post1_Sender
-    //     })
-    //     expect(response.statusCode).toEqual(400)
+    //     // //--- 
+    //     // response = await request(app).put('/post/new1').send({
+    //     //     "message": Post1_UpdatedMessage,
+    //     //     "sender": Post1_Sender
+    //     // })
+    //     // expect(response.statusCode).toEqual(400)
 
-    //     //--- 
-    //     response = await request(app).put('/post/' + Post1_ID).send({
-    //         "message": Post1_UpdatedMessage,
-    //     })
-    //     expect(response.statusCode).toEqual(200)
-    //     expect(response.body.message).toEqual(Post1_UpdatedMessage)
-    //     expect(response.body.sender).toEqual(Post1_Sender)
+    //     // //--- 
+    //     // response = await request(app).put('/post/' + Post1_ID).send({
+    //     //     "message": Post1_UpdatedMessage,
+    //     // })
+    //     // expect(response.statusCode).toEqual(200)
+    //     // expect(response.body.message).toEqual(Post1_UpdatedMessage)
+    //     // expect(response.body.sender).toEqual(Post1_Sender)
     // })
 
 });
