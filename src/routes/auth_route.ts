845ccbf265db24482b03304f8efadc37885df1e8
Anthony E <e4nthony@gmail.com>
2023-03-27 02:25:02 +0300

    swagger configured

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -2,24 +2,177 @@ import express from 'express'
 const router = express.Router()
 import Auth from '../controllers/auth.js'
 
+/**
+* @swagger
+* components:
+*   securitySchemes:
+*     bearerAuth:
+*       type: http
+*       scheme: bearer
+*       bearerFormat: JWT
+*/
 
+/**
+* @swagger
+* components:
+*   schemas:
+*     User:
+*       type: object
+*       required:
+*         - email
+*         - password
+*       properties:
+*         email:
+*           type: string
+*           description: The user email
+*         password:
+*           type: string
+*           description: The user password
+*       example:
+*         email: 'example@gmail.com'
+*         password: '1234ABcd!'
+*/
+
+/**
+* @swagger
+* tags:
+*   name: Auth
+*   description: The Authentication API.
+*/
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Register success retuns user info
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Registeration error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               err:
+ *                 type: string
+ *                 description: The error description 
+ *  
+ */
 router.post(
     '/register',
     Auth.register
 );
 
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Login success retuns access and refresh tokens.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               access_token:
+ *                 type: string
+ *                 description: The Access Token
+ *               refresh_token:
+ *                 type: string
+ *                 description: The Refresh Token
+ *             example:
+ *               access_token: 'eah458aser4h65ase4rh68...'
+ *               refresh_token: 'era65g1aer51g5aerg87...'
+ *       400:
+ *         description: Login error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               err:
+ *                 type: string
+ *                 description: The error description 
+ *
+ */
 router.post(
     '/login',
     Auth.login
 );
 
+/**
+ * @swagger
+ * /auth/logout:
+ *   get:
+ *     summary: Logout user, invalidate refresh_token.
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Logout sucess, refresh_token is invalidated
+ *       400:
+ *         description: Logout error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               err:
+ *                 type: string
+ *                 description: The error description 
+ */
 router.get(
     '/logout',
     Auth.logout
 );
 
 /**
- * refresh token route
+ * @swagger
+ * /auth/refresh:
+ *   get:
+ *     summary: refresh access token
+ *     tags: [Auth]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Refresh token success retuns access and refresh tokens
+ *         content:
+ *           application/json:
+ *             schema:
+ *               access_token:
+ *                 type: string
+ *                 description: The Access Token
+ *               refresh_token:
+ *                 type: string
+ *                 description: The Refresh Token
+ *             example:
+ *               access_token: '223412341...'
+ *               refresh_token: '123456...'
+ *       400:
+ *         description: Refresh token error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               err:
+ *                 type: string
+ *                 description: The error description 
+ *
  */
 router.get(
     '/refresh_auth',
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,46 @@
 import express from 'express'
 const app = express()
+
 import dotenv from 'dotenv'
 dotenv.config()
+
 import bodyParser from 'body-parser'
-app.use(bodyParser.urlencoded({extended:true, limit: '1mb'}))
+app.use(bodyParser.urlencoded({ extended: true, limit: '1mb' }))
 app.use(bodyParser.json())
 
 import mongoose from "mongoose"
 mongoose.connect(process.env.DATABASE_URL) //,{useNewUrlParser:true}) deleted because typescript error
 const db = mongoose.connection
-db.on('error',error=>{console.error('Failed to connect to MongoDB: '+error)})
-db.once('open',()=>{console.log('Connected to MongoDB.')})
+db.on('error', error => { console.error('Failed to connect to MongoDB: ' + error) })
+db.once('open', () => { console.log('Connected to MongoDB.') })
 
 //Make files in folder "public" accessible via url  //todo 
 app.use('/public', express.static('public'))
 
 import authRouter from './routes/auth_route.js'
-app.use('/auth',authRouter)
+app.use('/auth', authRouter)
 
 import postRouter from './routes/post_route.js'
-app.use('/post',postRouter)
+app.use('/post', postRouter)
+
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
+if (process.env.NODE_ENV == "development") {
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "REST Server - API",
+                version: "1.0.0",
+                description: "Web Development - SCE Cource 2022-2023. REST server including authentication using JWT.",
+            },
+            servers: [{ url: "http://localhost:4000", },],
+        },
+        apis: ["./src/routes/*.ts"],
+    };
+    const specs = swaggerJsDoc(options);
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+}
 
 
 export = app
