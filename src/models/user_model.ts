705fe18bddf940b088311182de40a6239e63efdb
Anthony E <e4nthony@gmail.com>
2023-03-26 03:14:26 +0300

    misc notes

--- src/models/user_model.ts
+++ src/models/user_model.ts
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    password: {
+    password: {         //encrypted password
         type: String,
         required: true
     }
