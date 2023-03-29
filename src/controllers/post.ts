c961dd29ab9b6a22b71dc6cbabd76f78b04599bb
Anthony E <e4nthony@gmail.com>
2023-03-29 05:17:35 +0300

    updated requests from app

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -7,7 +7,7 @@ const statusERROR = 400
 import { Request, Response } from 'express'
 import Post from '../models/post_model'
 
-const getPostById = async (req: any, res: any) => {
+const getPostById = async (req: Request, res: Response) => {
     // console.log(req.params.id);
 
     try {
@@ -57,34 +57,8 @@ const getAllPosts = async (req: Request, res: Response) => {
  * @param res : Response
  * @returns Response_cls
  */
-const addNewPost = async (req: Request | Request_cls) => {
-    console.log("addNewPost' request body:" + req.body.toString)
-
-    const post = new Post({
-        message: req.body.message,
-        sender: req.body.sender
-    });
-
-    try {
-        const newPost = await post.save();
-
-        console.log("save post in db (using classes)");
-
-        return new Response_cls(newPost, req.body.sender, null)
-
-        
-
-        // res.status(statusOK).send(newPost);
-    }
-    catch (err) {
-        console.log("failed to save post in DB (addNewPost())");
-        return new Response_cls(null, req.body.sender, new Error_cls(400, err.message))
-        // res.status(statusERROR).send({ 'error': 'fail adding new post to db' });
-    }
-}
-
-// const addNewPost = async (req: Request, res: Response) => {
-//     console.log("addNewPost' request body:" + req.body)
+// const addNewPost = async (req: Request | Request_cls) => {
+//     console.log("addNewPost' request body:" + req.body.toString)
 
 //     const post = new Post({
 //         message: req.body.message,
@@ -93,15 +67,41 @@ const addNewPost = async (req: Request | Request_cls) => {
 
 //     try {
 //         const newPost = await post.save();
-//         console.log("save post in db");
 
-//         res.status(statusOK).send(newPost);
+//         console.log("save post in db (using classes)");
+
+//         return new Response_cls(newPost, req.body.sender, null)
+
+        
+
+//         // res.status(statusOK).send(newPost);
 //     }
 //     catch (err) {
 //         console.log("failed to save post in DB (addNewPost())");
-//         res.status(statusERROR).send({ 'error': 'fail adding new post to db' });
+//         return new Response_cls(null, req.body.sender, new Error_cls(400, err.message))
+//         // res.status(statusERROR).send({ 'error': 'fail adding new post to db' });
 //     }
 // }
+
+const addNewPost = async (req: Request, res: Response) => {
+    console.log("addNewPost' request body:" + req.body)
+
+    const post = new Post({
+        message: req.body.message,
+        sender: req.body.sender
+    });
+
+    try {
+        const newPost = await post.save();
+        console.log("save post in db");
+
+        res.status(statusOK).send(newPost);
+    }
+    catch (err) {
+        console.log("failed to save post in DB (addNewPost())");
+        res.status(statusERROR).send({ 'error': 'fail adding new post to db' });
+    }
+}
 
 
 const putPostById = async (req: Request, res: Response) => {
